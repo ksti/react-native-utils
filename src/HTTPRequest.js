d4ca39138f18558a8d54fb56e33611aeacc76770
GJS <1353990812@qq.com>
2016-08-23 16:24:25 +0800

    now HTTPRequest support download and upload

--- src/HTTPRequest.js
+++ src/HTTPRequest.js
@@ -71,6 +71,16 @@ var _response = null
 var httpRequest = {
 
   /**
+   * 内置 XMLHttpRequest
+   */
+  xhr: new XMLHttpRequest(),
+
+  /**
+   * 取消状态
+   */
+  cancelled: false,
+
+  /**
    * 请求内容类型，对应设置不同的请求头和body
    */
   contentType: 'json',
@@ -296,6 +306,339 @@ var httpRequest = {
 
 
   },
+
+  /**
+   * @param {String} apiUrl
+   * @param {Object} parameter
+   * @param {Function} callback
+   */
+  download: function(apiUrl: string, parameter: Object, callback: Function) {
+    _response = null; // 清空response
+    var requestUrl = apiUrl;
+    if (parameter) { // parameter 直接进来就是json对象
+          /*
+          //jsonParameter = JSON.parse(parameter);
+          jsonParameter = JSON.stringify(parameter);
+          if (jsonParameter) {
+            console.log('---jsonParameter: ' + jsonParameter);
+            requestUrl += '?';
+            for(var key in parameter){
+              requestUrl += (key + '=' + parameter[key] + '&');
+            }
+            //encodeURIComponent(query)
+          }
+          */
+
+          requestUrl += '?';
+          for(var key in parameter){
+            requestUrl += (key + '=' + parameter[key] + '&');
+          }
+      //encodeURIComponent(query)
+    } else {
+      // With no query,
+    }
+    console.log('---requestUrl: ' + requestUrl);
+    console.log('---JSON.stringify(parameter): ' + JSON.stringify(parameter));
+
+    this._setDefualtHeader({
+      'Accept': 'text/plain, application/json, application/octet-stream',
+      'Accept-Charset': 'utf-8',
+    });
+
+    if (global.accessToken && global.accessToken.length > 0) {
+      this._setDefualtHeader({
+        'Accept': 'text/plain, application/json, application/octet-stream',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + global.accessToken,
+      });
+    }
+
+    if (this.headers) {
+      this.setRequestHeader(this.headers); // 在_setDefualtHeader之后调用，合并_defualtHeaders
+    };
+
+    //
+    console.log('post-header',this.headers?this.headers:this._defualtHeaders);
+
+    //
+    this.xhr && this.xhr.abort();
+
+    var xhr = this.xhr || new XMLHttpRequest();
+    var contentSize = 0;
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === xhr.HEADERS_RECEIVED) {
+        contentSize = parseInt(xhr.getResponseHeader('Content-Length'), 10);
+        console.log('HEADERS_RECEIVED.');
+      } else if (xhr.readyState === xhr.LOADING) {
+        console.log('LOADING...');
+        if (this.downloadProgress) {
+          if (xhr.responseType === 'text' || xhr.responseType === '') {
+            var responseText = xhr.responseText;
+            if (responseText) {
+              if (contentSize > 0) {
+                this.downloadProgress(responseText.length / contentSize);
+              };
+            }
+          }
+        };
+      } else if (xhr.readyState === xhr.DONE) {
+        console.log('DONE!');
+        if (this.cancelled) {
+          this.cancelled = false;
+          return;
+        }
+        if (xhr.status === 200) {
+          alert('Download complete!');
+        } else if (xhr.status !== 0) {
+          alert('Error: Server returned HTTP status of ' + xhr.status + ' ' + xhr.responseText);
+        } else {
+          alert('Error: ' + xhr.responseText);
+        }
+      }
+    };
+
+    // xhr.onprogress = (oEvent) => {
+    //   if (oEvent.lengthComputable) {
+    //     var percentComplete = oEvent.loaded / oEvent.total;
+    //     // ...
+    //     if (this.updateProgress) {
+    //       this.updateProgress(percentComplete, oEvent.currentTarget);
+    //     };
+    //   } else {
+    //     // Unable to compute progress information since the total size is unknown
+    //   }
+    // }
+
+    xhr.onload = (oEvent) => {
+      _response = xhr;
+      if (xhr.responseType === 'arraybuffer' || xhr.responseType === 'blob') {
+        var arrayBuffer = xhr.response; // Note: not oReq.responseText
+        // var blob = xhr.response; // arraybuffer 和 blob 都可以
+        if (arrayBuffer) {
+          var byteArray = new Uint8Array(arrayBuffer);
+          // for (var i = 0; i < byteArray.byteLength; i++) {
+          //   // do something with each byte in the array
+          // }
+          if (callback) {
+              callback(null, byteArray, _response); // (error, responseData, response)
+          };
+        }
+      } else if (xhr.responseType === 'text' || xhr.responseType === '' || xhr.responseType === 'json') {
+        var responseText = xhr.responseText;
+        if (callback) {
+            callback(null, responseText, _response); // (error, responseData, response)
+        };
+      } else {
+        if (callback) {
+            callback(null, xhr.response, _response); // (error, responseData, response)
+        };
+      }
+      
+    };
+
+    xhr.addEventListener("progress", updateProgress.bind(this));
+    xhr.addEventListener("load", transferComplete.bind(this));
+    xhr.addEventListener("error", transferFailed.bind(this));
+    xhr.addEventListener("abort", transferCanceled.bind(this));
+
+    // ...
+
+    // progress on transfers from the server to the client (downloads)
+    function updateProgress (oEvent) {
+      if (oEvent.lengthComputable) {
+        var percentComplete = oEvent.loaded / oEvent.total;
+        // ...
+        if (this.updateProgress) {
+          this.updateProgress(percentComplete, oEvent.currentTarget);
+        };
+      } else {
+        // Unable to compute progress information since the total size is unknown
+      }
+    }
+
+    function transferComplete(evt) {
+      console.log("The transfer is complete.");
+      if (this.transferComplete) {
+        this.transferComplete(evt.currentTarget.responseText, evt.currentTarget);
+      };
+    }
+
+    function transferFailed(evt) {
+      console.log("An error occurred while transferring the file.");
+      if (this.transferFailed) {
+        this.transferFailed(evt.currentTarget.responseText, evt.currentTarget);
+      };
+    }
+
+    function transferCanceled(evt) {
+      console.log("The transfer has been canceled by the user.");
+      if (this.transferCanceled) {
+        this.transferCanceled(evt.currentTarget.responseText, evt.currentTarget);
+      };
+    }
+
+    xhr.open('GET', requestUrl);
+    var _allHeaders = this.headers?this.headers:this._defualtHeaders;
+    Object.keys(_allHeaders).map((id, index) => {
+        xhr.setRequestHeader(id, _allHeaders[id]);
+    })
+    // xhr.responseType = "arraybuffer"; // arraybuffer
+    // xhr.responseType = "blob"; // blob
+    // xhr.responseType = "text"; // text
+    xhr.responseType = this.responseType;
+        
+    xhr.send();
+    this.xhr = xhr;
+
+  },
+
+  /**
+   * @param {String} apiUrl
+   * @param {Object} parameter
+   * @param {Function} callback
+   */
+  upload: function(apiUrl: string, parameter: Object, callback: Function) {
+    _response = null; // 清空response
+
+    var requestUrl = apiUrl;
+
+    console.log('---requestUrl: ' + requestUrl);
+    console.log('---contentType: ' + this.contentType);
+    console.log('---JSON.stringify(parameter): ' + JSON.stringify(parameter));
+
+    var headerParams = this._defualtHeaders;
+    var bodyParams = JSON.stringify(parameter);
+    if (this.contentType == 'form') {
+      headerParams = {
+        'Accept': 'application/json',
+        'Accept-Charset': 'utf-8',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      };
+      bodyParams = toQueryString(parameter);
+    }
+
+    let accessToken = global.accessToken;
+    if (accessToken && accessToken.length > 0) {
+        headerParams.Authorization = 'Bearer '+ accessToken;
+    }
+
+    this._setDefualtHeader(headerParams);
+    if (this.headers) {
+      this.setRequestHeader(this.headers); // 在_setDefualtHeader之后调用，合并_defualtHeaders
+    };
+
+    console.log('post-header', this.headers?this.headers:this._defualtHeaders);
+
+    _upload = () => {
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', requestUrl);
+      
+      var _allHeaders = this.headers?this.headers:this._defualtHeaders;
+      Object.keys(_allHeaders).map((id, index) => {
+          xhr.setRequestHeader(id, _allHeaders[id]);
+      });
+
+      xhr.onload = () => {
+        if (xhr.status !== 200) {
+          alert(
+            'Upload failed',
+            'Expected HTTP 200 OK response, got ' + xhr.status
+          );
+          return;
+        }
+        if (!xhr.responseText) {
+          alert(
+            'Upload failed',
+            'No response payload.'
+          );
+          return;
+        }
+        var index = xhr.responseText.indexOf('http://www.posttestserver.com/');
+        if (index === -1) {
+          alert(
+            'Upload failed',
+            'Invalid response payload.'
+          );
+          return;
+        }
+        // var url = xhr.responseText.slice(index).split('\n')[0];
+        // Linking.openURL(url);
+      };
+
+      var formdata = new FormData();
+      // if (this.state.randomPhoto) {
+      //   formdata.append('image', {...this.state.randomPhoto, name: 'image.jpg'});
+      // }
+
+      Object.keys(parameter).map((id, index) => {
+          formdata.append(id, parameter[id]);
+      });
+
+      if (xhr.upload) {
+        xhr.upload.onprogress = (event) => {
+          console.log('upload onprogress', event);
+          // // if (event.lengthComputable) {
+          // //   this.setState({uploadProgress: event.loaded / event.total});
+          // // }
+          if (event.lengthComputable) {
+            var percentComplete = event.loaded / event.total;
+            // ...
+            if (this.updateProgress) {
+              this.updateProgress(percentComplete, event.currentTarget);
+            };
+          } else {
+            // Unable to compute progress information since the total size is unknown
+          }
+        };
+      }
+
+      xhr.addEventListener("progress", updateProgress.bind(this));
+      xhr.addEventListener("load", transferComplete.bind(this));
+      xhr.addEventListener("error", transferFailed.bind(this));
+      xhr.addEventListener("abort", transferCanceled.bind(this));
+
+      // ...
+
+      // progress on transfers from the server to the client (downloads)
+      function updateProgress (oEvent) {
+        if (oEvent.lengthComputable) {
+          var percentComplete = oEvent.loaded / oEvent.total;
+          // ...
+          if (this.updateProgress) {
+            this.updateProgress(percentComplete, oEvent.currentTarget);
+          };
+        } else {
+          // Unable to compute progress information since the total size is unknown
+        }
+      }
+
+      function transferComplete(evt) {
+        console.log("The transfer is complete.");
+        if (this.transferComplete) {
+          this.transferComplete(evt.currentTarget.responseText, evt.currentTarget);
+        };
+      }
+
+      function transferFailed(evt) {
+        console.log("An error occurred while transferring the file.");
+        if (this.transferFailed) {
+          this.transferFailed(evt.currentTarget.responseText, evt.currentTarget);
+        };
+      }
+
+      function transferCanceled(evt) {
+        console.log("The transfer has been canceled by the user.");
+        if (this.transferCanceled) {
+          this.transferCanceled(evt.currentTarget.responseText, evt.currentTarget);
+        };
+      }
+          
+      xhr.send(formdata);
+      this.xhr = xhr;
+    }
+
+    _upload();
+  }
 
 };
 
