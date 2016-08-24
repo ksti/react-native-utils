ff2dea0f383f584779b03ae00702c13489cce6f5
kstain <1353990812@qq.com>
2016-08-24 11:04:27 +0800

    Update FormView.js

--- src/FormView.js
+++ src/FormView.js
@@ -24,7 +24,7 @@
  * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
- * @providesModule httpRequest
+ * @providesModule FormView
  * @flow
  */
 
