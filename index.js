e529dfe0df6cac6a85f5edca773536db606e4c83
GJS <1353990812@qq.com>
2016-09-05 16:37:28 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 import httpRequest from './src/httpRequest';
 import FormView from './src/FormView';
+import FormView from './src/customPopupPage/PopupPage';
+import FormView from './src/PopupView/PopupSelecter';
 
 export {
   httpRequest,
