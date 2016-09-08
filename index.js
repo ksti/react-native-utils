f366cd40d94ae0df1f355f8ebb2586cc756ba5c9
GJS <1353990812@qq.com>
2016-09-08 19:27:58 +0800

    export Form for using FormView

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ import FormView from './src/components/FormView/FormView';
 import {storageUtil} from './src/utils/storage'
 import PopupPage from './src/components/customPopupPage/PopupPage';
 import PopupSelecter from './src/components/PopupView/PopupSelecter';
+import Form from 'react-native-form'
 
 export {
   httpRequest,
+  Form,
   FormView,
   storageUtil,
   PopupPage,
