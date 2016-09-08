b80c5c67fe967316d18e776dea006f7ce603731a
GJS <1353990812@qq.com>
2016-09-08 19:53:40 +0800

    index export Form from 'react-native-form'

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 
 import httpRequest from './src/utils/httpRequest';
 import FormView from './src/components/FormView/FormView';
+import PageContainer from './src/components/FormView/PageContainer';
+import ScrollContainer from './src/components/FormView/ScrollContainer';
 import {storageUtil} from './src/utils/storage'
 import PopupPage from './src/components/customPopupPage/PopupPage';
 import PopupSelecter from './src/components/PopupView/PopupSelecter';
-import Form from 'react-native-form'
+import Form from 'react-native-form';
 
 export {
   httpRequest,
   Form,
   FormView,
+  PageContainer,
+  ScrollContainer,
   storageUtil,
   PopupPage,
   PopupSelecter
