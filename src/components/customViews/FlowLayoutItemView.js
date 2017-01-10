75e4a9bdaaa29a2f4c291781d11f4ed265ca3386
GJS <1353990812@qq.com>
2017-01-10 10:28:22 +0800

    add corner mark for items

--- src/components/customViews/FlowLayoutItemView.js
+++ src/components/customViews/FlowLayoutItemView.js
@@ -86,6 +86,92 @@ export default class FlowLayoutItemView extends Component {
         this.dataSource = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     }
 
+    // 渲染角标容器
+    _renderCornerMarkContainer = (children, cornerMark) => {
+        return (
+            <View style={[{padding: 10}, this.props.CornerMarkContainerStyle]}>
+                {children}
+                {this._renderCornerMark(cornerMark, this.props.onPressCornerMark)}
+            </View>
+        )
+    }
+
+    // 渲染(右上角)角标
+    _renderCornerMark = (cornerMark, onPressCornerMark) => {
+        let cornerMarkView = null;
+        if (cornerMark) {
+            switch (typeof cornerMark) {
+                case 'string': // 字符串
+                    cornerMarkView = (
+                        <View style={
+                            [
+                                {
+                                    alignItems: 'center', justifyContent: 'center',
+                                    borderColor: 'white', borderWidth: 1, borderRadius: 10,
+                                    overflow: 'hidden',
+                                },
+                                {minWidth: 20, height: 20},
+                                {backgroundColor: 'red'},
+                                this.props.cornerMarkViewStyle,
+                            ]
+                        }
+                        >
+                            <Text style={[{color: 'white'}, this.props.cornerMarkTextStyle]}>{cornerMark}</Text>
+                        </View>
+                    );
+                    break;
+                case 'number': // 数字
+                    if (cornerMark > 0) {
+                        cornerMarkView = (
+                            <View style={
+                                [
+                                    {
+                                        alignItems: 'center', justifyContent: 'center',
+                                        borderColor: 'white', borderWidth: 1, borderRadius: 10,
+                                        overflow: 'hidden',
+                                    },
+                                    {minWidth: 20, height: 20},
+                                    {backgroundColor: 'red'},
+                                    this.props.cornerMarkViewStyle,
+                                ]
+                            }
+                            >
+                                <Text style={[{color: 'white'}, this.props.cornerMarkTextStyle]}>{cornerMark}</Text>
+                            </View>
+                        );
+                    };                    
+                    break;
+                case 'object': // 对象
+                    cornerMarkView = cornerMark;
+                    break;
+
+                default:
+                    break;
+            };
+        };
+
+        // 
+        if (cornerMarkView) {
+            return(
+                <TouchableOpacity
+                    onPress={() => onPressCornerMark && onPressCornerMark()}
+                    style={
+                        [
+                            {position: 'absolute', top: 0, right: 0},
+                            this.props.cornerMarkStyle,
+                        ]
+                    }
+                >
+                    {cornerMarkView}
+                </TouchableOpacity>
+            )
+        }else{
+            return(
+                <View />    
+            )
+        }
+    }
+
     _renderFlowLayoutList = () => {
       return(
         <View
@@ -95,10 +181,17 @@ export default class FlowLayoutItemView extends Component {
               enableEmptySections={true}
               contentContainerStyle={styles.list}
               dataSource={this.dataSource.cloneWithRows(this.props.dataSource || [])}
-              renderRow={this._renderRow}
+              renderRow={this._renderRowCornerMark}
           />
         </View>
       )
+    }
+
+    /*
+     * 渲染带角标的item
+    */
+    _renderRowCornerMark = (rowData, sectionID, rowID) => {
+        return this._renderCornerMarkContainer(this._renderRow(rowData, sectionID, rowID), rowData.number);
     }
 
     /*
@@ -122,7 +215,7 @@ export default class FlowLayoutItemView extends Component {
                 key={`${rowID}`}
                 style={{ backgroundColor: "#ffffff", }}
                 underlayColor="#ffffff"
-                onPress={() => { this.props.onPress &&this.props.onPress(rowData, sectionID, rowID) } }>
+                onPress={() => { this.props.onPress && this.props.onPress(rowData, sectionID, rowID) } }>
                 <View
                     style={{ width: equalWidth, height: equalHeight, alignItems: 'center', margin: margin, marginBottom: 10, }}>
                     <Image
